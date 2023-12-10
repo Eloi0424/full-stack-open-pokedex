fcb04d8ee7e55405857ae9a39a0ca57dd536711c
Eloi0424 <beichu0424@qq.com>
2023-12-10 17:59:22 +0800

    Exercise 11.10

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
+
+app.get("/api/version", (req, res) => {
+  res.send("1.0");
+})
+
+
 app.listen(PORT, () => {
   console.log("server started on port 5000");
 });
